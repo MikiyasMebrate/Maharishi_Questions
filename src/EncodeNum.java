dbab0e55756b7cbd8f2c3d48b2f97e5aff79a5ef
Mikiyas Mebrate <mikiyasmebrate@gmail.com>
2022-10-27 17:47:57 +0300

    EncodeNum

--- src/EncodeNum.java
+++ src/EncodeNum.java
@@ -1,36 +1,42 @@
-public class EncodeNum {
+import java.util.Arrays;
 
+public class EncodeNum {
     static int[] encodeNumber (int n) {
         int org = n;
         int count = 0;
-        if (n <= 0) return null;
+        if (n <= 1) return null;
         else {
             int i = 2;
-            while (i < n) {
-                if (n % i == 0) {
+            while (i <= n) {
+                while(n % i == 0) {
                     count++;
-                    n /= i;
-                    continue;
+                    n = n/i;
                 }
                 i++;
             }
-        }
-        int[] a = new int[count];
-        int j = 2,in=0;
-        while (j < n) {
-            if (n % j == 0) {
-                a[in] = j;
-                n /= j;
-                continue;
-            }
-            j++;
-        }
 
-        return a;
+            int[] a = new int[count];
+            int j = 2,in=0;
+            while (j <= org) {
+                while (org % j == 0) {
+                    a[in] = j;
+                    org = org/j;
+                    in++;
+                }
+                j++;
+            }
+            return a;
+        }
     }
 
 
-    public static void main(){
-        System.out.println(encodeNumber(1200));
+    public static void main(String argus[]){
+        System.out.println(Arrays.toString(encodeNumber(2)));
+        System.out.println(Arrays.toString(encodeNumber(6)));
+        System.out.println(Arrays.toString(encodeNumber(14)));
+        System.out.println(Arrays.toString(encodeNumber(24)));
+        System.out.println(Arrays.toString(encodeNumber(1200)));
+        System.out.println(Arrays.toString(encodeNumber(1)));
+        System.out.println(Arrays.toString(encodeNumber(-18)));
     }
 }
